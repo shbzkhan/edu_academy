3177840eb7f9f536a495ac4e56156f80f6f4d9c0
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2025-06-28 10:59:49 -0700

    setup project

---
+++ backend/models/user.models.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+        },
+        courses: [
+            {
+                type: Schema.Types.ObjectId,
+                ref:"courses"
+        }
+    ]
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const Users = mongoose.model("Users", userSchema);
